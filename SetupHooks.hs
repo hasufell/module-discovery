6ecadfa79efd2a3a80b72021845f67f742fb0544
Julian Ospald <hasufell@posteo.de>
2024-06-08 22:37:10 +0800

    Update

--- SetupHooks.hs
+++ SetupHooks.hs
@@ -5,10 +5,12 @@ module SetupHooks where
 import Control.Monad
 import Data.List.Extra
 import Data.String
+import Distribution.ModuleName
 import Distribution.Simple.SetupHooks
 import Distribution.Utils.Path
 import System.FilePath
 import System.FilePattern.Directory
+import qualified Data.Set as Set
 
 
 setupHooks :: SetupHooks
@@ -18,16 +20,27 @@ setupHooks =
    }
 
 myConfigureHooks :: ConfigureHooks
-myConfigureHooks =
- noConfigureHooks
-   { preConfComponentHook = Just $ \PreConfComponentInputs{..} ->
-       case component of
-         CLib (lib@Library { libName = LMainLibName, libBuildInfo = bi }) -> do
-           let dirs = interpretSymbolicPath Nothing <$> hsSourceDirs bi
-           moduleFiles <- fmap mconcat $ forM dirs $ \dir -> getDirectoryFiles dir ["**/*.hs","**/*.hsc"]
-           let modules = fromString . replace "/" "." . dropExtension <$> moduleFiles
-           pure (PreConfComponentOutputs (ComponentDiff (CLib lib{ exposedModules = modules })))
-         lib -> do
-           pure (PreConfComponentOutputs (ComponentDiff component))
-   }
+myConfigureHooks = noConfigureHooks
+  { preConfComponentHook = Just $ \PreConfComponentInputs{..} -> case component of
+      CLib (lib@Library { libName = LMainLibName, libBuildInfo = bi }) -> do
+        let sourceDirs = interpretSymbolicPath Nothing <$> hsSourceDirs bi
+        moduleFiles <- fmap mconcat $ forM sourceDirs getHaskellFiles
+        let existingModules = Set.fromList $ exposedModules lib
+            discoveredModules = Set.fromList
+                                  $ filePathToModuleName
+                                 <$> moduleFiles
+            distinctModules = discoveredModules `Set.difference` existingModules
+        pure
+          $ PreConfComponentOutputs
+              (ComponentDiff
+                (CLib lib{ exposedModules = Set.toList distinctModules }))
+      lib -> do
+        pure (PreConfComponentOutputs (ComponentDiff component))
+  }
+ where
+  filePathToModuleName :: FilePath -> ModuleName
+  filePathToModuleName = fromString . replace "/" "." . dropExtension
+
+  getHaskellFiles :: FilePath -> IO [FilePath]
+  getHaskellFiles sourceDir = getDirectoryFiles sourceDir ["**/*.hs","**/*.hsc"]
 
